7930f09490543d7127f79c3123ddc33e56ac2d1b
licantropia <110132164+gcorona72@users.noreply.github.com>
2025-03-24 17:49:03 +0100

    Initial commit

---
+++ src/main/java/org/example/problemparada/ProblemaDeLaParadaApplication.java
@@ -0,0 +1,13 @@
+package org.example.problemparada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemaDeLaParadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemaDeLaParadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/problemparada/ProblemaDeLaParadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.problemparada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemaDeLaParadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
